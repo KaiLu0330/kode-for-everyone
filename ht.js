38df5446ece4885d2a964fc79bbd212056f0db39
KaiLu0330 <116818875+KaiLu0330@users.noreply.github.com>
2022-12-16 22:27:27 +0800

    Add files via upload

---
+++ ht.js
@@ -0,0 +1,21 @@
+/*function click(){
+    alert("登入失敗");
+}
+function click2(){
+    alert("登入失敗");
+}
+function find(){
+    alert("error");
+}*/
+var btn=document.getElementById("btn");
+btn.addEventListener("click",function(){
+    alert("登入失敗")
+})
+var btn2=document.getElementById("btn2");
+btn.addEventListener("click",function(){
+    alert("註冊失敗")
+})
+var btn3=document.getElementById("btn3");
+btn.addEventListener("click",function(){
+    alert("error")
+})
